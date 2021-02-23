a8462e0ef2af65f434074943f710e48359ab8d1c
NizarNaufal <32515100+NizarNaufal@users.noreply.github.com>
2021-02-23 14:07:08 +0700

    create calculator swift

--- UIKitCalculatorSwift/ViewController.swift
+++ UIKitCalculatorSwift/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var width: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateArea(_ sender: UIButton) {
+        if let h = Double(height.text ?? "0"),let w = Double(width.text ?? "0"){
+            result.text = String(h*w)
+        }else{
+            result.text = "Hasil Tidak Valid"
+        }
+    }
+    
+    @IBAction func resetView(_ sender: UIButton) {
+        result.text = ""
+        height.text = ""
+        width.text = ""
+    }
 }
 
